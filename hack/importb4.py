1dd711b250ab3a6f3d3ec114a907327ecb56fef2
Agatha Barros <agathabarros@gmail.com>
2023-10-24 20:56:48 +0100

    a

--- hack/importb4.py
+++ hack/importb4.py
@@ -5,6 +5,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 
 servico = Service(ChromeDriverManager().install())
@@ -13,22 +16,31 @@ navegador = webdriver.Chrome(service=servico)
 #interagir com elementos
 
 
-
+#https://www.elcorteingles.pt/supermercado/
+#https://www.garrafeirasoares.pt/pt/
+#
 html = "https://www.continente.pt/"
 navegador.get(html)
 
 try:
-    #tratamento cookie
-    element = navegador.find_element(By.XPATH, '//*[@id="CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll"]').click()
+    #tratamento cooki continet
+    cookie_button = navegador.find_element(By.XPATH, '//*[@id="CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll"]').click()
+    #tratamento cooki continet
+    button = navegador.find_element(By.CSS_SELECTOR, 'button[aria-label="Introduza o termo que deseja pesquisar"]').click()
+    search_input = navegador.find_element(By.CSS_SELECTOR, 'input[aria-label="Introduza o termo que deseja pesquisar"]').send_keys("5601012001310")
+
+
+
+
+
 except Exception as e:
     print(f"Ocorreu um erro: {e}")
 
-    list_elements = navegador.find_elements(By.XPATH, '//*[@id="brand-header"]/nav/div/div[1]')
-    for elements in list_elements:
-        if "search" in list_elements.text.lower():
-            elements.click()
-            break
+    # Click on the search icon\
 
+
+    
+    
     
     
      
